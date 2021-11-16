99232051f8bd5b2a0dc1a2890a1d3a92d2ec4c67
Sandipan Ghosh <sandipanghosh64@gmail.com>
2021-11-16 09:41:14 +0530

    initialized

---
+++ frontend-react/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
